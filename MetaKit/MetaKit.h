17f79f653d891420d4a40a9da42e11d3625f1137
florianmaxim <hello@florianmaxim.com>
2018-08-04 02:07:53 +0200

    Hello meta objects

---
+++ MetaKit/MetaKit.h
@@ -0,0 +1,19 @@
+//
+//  MetaKit.h
+//  MetaKit
+//
+//  Created by Florian on 04/08/2018.
+//  Copyright © 2018 Florian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetaKit.
+FOUNDATION_EXPORT double MetaKitVersionNumber;
+
+//! Project version string for MetaKit.
+FOUNDATION_EXPORT const unsigned char MetaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetaKit/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+#  Meta for iOS
+
+## Super-high level XR (VR/AR) API for iOS.
+
+Introducing meta objects.
+
+
+
